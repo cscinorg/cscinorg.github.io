2090b9e7370a1a1f0a4cbf45003e43d47def6e37
K P Bhat <kpbhat@tipf.in>
2025-08-24 18:46:18 +0530

    public folder copied from local

---
+++ public/searchindex.en.js
@@ -0,0 +1,66 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Home \u003e Documentation",
+    "content": "The digitalisation initiatives across India have increased significantly, bringing enhanced national productivity, efficiency, growth and quality of service. At the same time, a manifold increase in adverse cyber incidents pose serious danger to the nation’s well-being. Cyber-attacks on critical information infrastructure by organised criminal groups, non-state and state sponsored groups can cause debilitating impact to national security, economy, public health and public safety.\nThe critical sector entities (CSEs) that own, run, and manage their critical information infrastructure (CII) are solely responsible for the protection of their computer resources (systems, networks, applications, data repositories and identities). The entities are expected and required to use appropriate technology, implement various best practices and build a competent workforce to achieve a high level of cyber resilience.\nCSEs and other organisations would benefit from a generic framework for cyber resilience that harmonises the government and regulatory guidelines, directives and global best practices.\nFederated System Covergence of Perspectives Every organisation has a complex mix of stakeholders, both internal and external, each having their own specific roles, functions, backgrounds, competence and limitations. All stakeholders should be able to communicate, both orally and written, their respective perspectives clearly for others to understand. A well-defined and comprehensive “Glossary of terms and definitions” will enable all the stakeholders to communicate their respective viewpoints without ambiguity and misunderstanding, leading to faster and better convergence of perspectives and improving the decision process at all levels.",
+    "description": "The digitalisation initiatives across India have increased significantly, bringing enhanced national productivity, efficiency, growth and quality of service. At the same time, a manifold increase in adverse cyber incidents pose serious danger to the nation’s well-being. Cyber-attacks on critical information infrastructure by organised criminal groups, non-state and state sponsored groups can cause debilitating impact to national security, economy, public health and public safety.\nThe critical sector entities (CSEs) that own, run, and manage their critical information infrastructure (CII) are solely responsible for the protection of their computer resources (systems, networks, applications, data repositories and identities). The entities are expected and required to use appropriate technology, implement various best practices and build a competent workforce to achieve a high level of cyber resilience.",
+    "tags": [],
+    "title": "Introduction",
+    "uri": "/docs/introduction/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "Critical sector entities and other organisations have no dearth of information and guidance from multiple sources to help them secure their IT systems and networks. Yet, a large number of them continue to suffer downtime and losses due to cyber-attacks. An analysis of successful attacks indicate that most of them succeeded because the entities did not or could not apply the cybersecurity information and guidance effectively.\nThe documentation in this section attempts to address the gap between knowledge and execution. Many of the insights and perspectives in this section are derived from the practices and processes adopted by organisations having a high level of cybersecurity capability and maturity.\nOrganisations should take this documentation as a Framework that needs to be adapted for their specific requirements.",
+    "description": "Critical sector entities and other organisations have no dearth of information and guidance from multiple sources to help them secure their IT systems and networks. Yet, a large number of them continue to suffer downtime and losses due to cyber-attacks. An analysis of successful attacks indicate that most of them succeeded because the entities did not or could not apply the cybersecurity information and guidance effectively.\nThe documentation in this section attempts to address the gap between knowledge and execution. Many of the insights and perspectives in this section are derived from the practices and processes adopted by organisations having a high level of cybersecurity capability and maturity.",
+    "tags": [],
+    "title": "Overview",
+    "uri": "/docs/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This website has information related to cybersecurity and cyber resilience that is primarily meant for critical sector entities (CSEs) of India. However, the information is usable by entities of other sectors too.\nThe Introduction section under Documentation provides a detailed perspective of what this website intends to achieve.",
+    "description": "This website has information related to cybersecurity and cyber resilience that is primarily meant for critical sector entities (CSEs) of India. However, the information is usable by entities of other sectors too.\nThe Introduction section under Documentation provides a detailed perspective of what this website intends to achieve.",
+    "tags": [],
+    "title": "Home",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "Under Construction",
+    "description": "Contact details",
+    "tags": [],
+    "title": "Contact",
+    "uri": "/contact/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "FAQs",
+    "tags": [],
+    "title": "FAQs",
+    "uri": "/faqs/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "Under construction",
+    "description": "Downloadable resources",
+    "tags": [],
+    "title": "Resources",
+    "uri": "/resources/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
