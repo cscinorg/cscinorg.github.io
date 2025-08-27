af336dd5841950d72960588fa8bd0138323e5f09
K P Bhat <kpbhat@tipf.in>
2025-08-27 22:47:29 +0530

    27Aug25 docs folder updated

--- docs/searchindex.en.js
+++ docs/searchindex.en.js
@@ -1,59 +1,107 @@
 var relearn_searchindex = [
   {
     "breadcrumb": "Home \u003e Documentation",
-    "content": "The digitalisation initiatives across India have increased significantly, bringing enhanced national productivity, efficiency, growth and quality of service. At the same time, a manifold increase in adverse cyber incidents pose serious danger to the nation’s well-being. Cyber-attacks on critical information infrastructure by organised criminal groups, non-state and state sponsored groups can cause debilitating impact to national security, economy, public health and public safety.\nThe critical sector entities (CSEs) that own, run, and manage their critical information infrastructure (CII) are solely responsible for the protection of their computer resources (systems, networks, applications, data repositories and identities). The entities are expected and required to use appropriate technology, implement various best practices and build a competent workforce to achieve a high level of cyber resilience.\nCSEs and other organisations would benefit from a generic framework for cyber resilience that harmonises the government and regulatory guidelines, directives and global best practices.\nFederated System Covergence of Perspectives Every organisation has a complex mix of stakeholders, both internal and external, each having their own specific roles, functions, backgrounds, competence and limitations. All stakeholders should be able to communicate, both orally and written, their respective perspectives clearly for others to understand. A well-defined and comprehensive “Glossary of terms and definitions” will enable all the stakeholders to communicate their respective viewpoints without ambiguity and misunderstanding, leading to faster and better convergence of perspectives and improving the decision process at all levels.",
-    "description": "The digitalisation initiatives across India have increased significantly, bringing enhanced national productivity, efficiency, growth and quality of service. At the same time, a manifold increase in adverse cyber incidents pose serious danger to the nation’s well-being. Cyber-attacks on critical information infrastructure by organised criminal groups, non-state and state sponsored groups can cause debilitating impact to national security, economy, public health and public safety.\nThe critical sector entities (CSEs) that own, run, and manage their critical information infrastructure (CII) are solely responsible for the protection of their computer resources (systems, networks, applications, data repositories and identities). The entities are expected and required to use appropriate technology, implement various best practices and build a competent workforce to achieve a high level of cyber resilience.",
+    "content": "",
+    "description": "Documentation related to cyberspace and federated digital ecosystem",
     "tags": [],
-    "title": "Introduction",
-    "uri": "/docs/introduction/index.html"
-  },
-  {
-    "breadcrumb": "Home",
-    "content": "Critical sector entities and other organisations have no dearth of information and guidance from multiple sources to help them secure their IT systems and networks. Yet, a large number of them continue to suffer downtime and losses due to cyber-attacks. An analysis of successful attacks indicate that most of them succeeded because the entities did not or could not apply the cybersecurity information and guidance effectively.\nThe documentation in this section attempts to address the gap between knowledge and execution. Many of the insights and perspectives in this section are derived from the practices and processes adopted by organisations having a high level of cybersecurity capability and maturity.\nOrganisations should take this documentation as a Framework that needs to be adapted for their specific requirements.",
-    "description": "Critical sector entities and other organisations have no dearth of information and guidance from multiple sources to help them secure their IT systems and networks. Yet, a large number of them continue to suffer downtime and losses due to cyber-attacks. An analysis of successful attacks indicate that most of them succeeded because the entities did not or could not apply the cybersecurity information and guidance effectively.\nThe documentation in this section attempts to address the gap between knowledge and execution. Many of the insights and perspectives in this section are derived from the practices and processes adopted by organisations having a high level of cybersecurity capability and maturity.",
-    "tags": [],
-    "title": "Overview",
-    "uri": "/docs/index.html"
+    "title": "Ecosystem related documentation",
+    "uri": "/docs/ecosystem/index.html"
   },
   {
     "breadcrumb": "",
-    "content": "This website has information related to cybersecurity and cyber resilience that is primarily meant for critical sector entities (CSEs) of India. However, the information is usable by entities of other sectors too.\nThe Introduction section under Documentation provides a detailed perspective of what this website intends to achieve.",
-    "description": "This website has information related to cybersecurity and cyber resilience that is primarily meant for critical sector entities (CSEs) of India. However, the information is usable by entities of other sectors too.\nThe Introduction section under Documentation provides a detailed perspective of what this website intends to achieve.",
+    "content": "Purpose The purpose of this site is to inform and guide the leadership, management and the composite workforce of critical sector entities (CSEs) on various aspects related to the use, security and resilience of their digital ecosystem. The content is also useful to entities of other sectors too.\nBackground Over the last two decades, India has transformed itself into a highly digitalised nation that has embedded Information and Communication Technology (ICT), Operational Technology (OT) and Internet of Things (IoT) deeply into almost all aspects of our lives. We now have a vibrant national cyberspace, which manifests through a digital ecosystem formed by geographically dispersed, interconnected and federated conglomeration of systems, networks, data repositories, applications, devices, processes and services.\nThe cyberspace is vital to national security, economy, public health, public safety and overall national well-being. The digital ecosystem enables critical sector entities and other organisations to deliver the national critical functions through business and industrial processes that use the digital infrastructure. Hence, it is essential to build, maintain and continually improve the resilience of the digital ecosystem of our critical sectors through a coordinated approach. Any loss of trust in the same has severe consequences for users and other stakeholders of the national cyberspace.\nThe IT Act 2000 (amended 2008) applies to users and providers of the digital ecosystem, including home users, MSMEs, large enterprises, government \u0026 non-government entities. It was framed to help secure the digital ecosystem while in use by the government, businesses and others for their service delivery, business and organisational functions.\nThe IT Act recognises that the basic responsibility for protecting an entity’s information infrastructure lies with the entity itself. It envisages the possible threats to the digital ecosystem during its use and provides direction and guidance for its protection. Relevant Rules have been framed under the IT Act to enable designated cybersecurity agencies like NCIIPC and CERT-In to oversee, monitor and respond to cyber attacks from malicious threat actors onto the critical information infrastructure and the larger digital ecosystem. There are other Acts and Rules that mandate sector regulators and other authorities to provide regulatory frameworks to secure the critical sectors and build cyber resilience in critical sector entities under their jurisdiction.\nStructure The site’s documentation section is divided into multiple chapters and topics that reflect the perspectives, concerns and needs of different stakeholders within and across critical sector entities and other organisations.\nThe Notes, FAQs, Resources and Glossary sections will be populated over time. A Discussions platform is also planned to be set up in parallel for feedback and community engagement.\nContext Cyber-attacks on critical information infrastructure by organised criminal groups, non-state and state sponsored groups can cause debilitating impact to national security, economy, public health and public safety. The critical sector entities (CSEs) that own, run, and manage their critical information infrastructure (CII) are responsible for the protection of their computer resources (systems, networks, applications, data repositories and identities). The entities are expected and required to use appropriate technology, implement various best practices and build a competent workforce to achieve a high level of cyber resilience.\nThe CSEs and other organisations have no dearth of information and guidance from multiple sources to help them secure their IT systems and networks. Yet, a large number of them continue to suffer downtime and losses due to cyber-attacks. An analysis of successful attacks indicate that most of them succeeded because the entities did not or could not apply the cybersecurity information and guidance effectively. A common refrain from cybersecurity experts and empanelled auditors is that a large number of entities have negligible or poor implementation of basic and essential cybersecurity controls. It looks as if the entities “do not understand” or “do not want to implement” cybersecurity.\nInteraction with business leaders, managers and the non-IT workforce of entities provides a different perspective. The underlying message from these interactions is that they are unable to integrate cybersecurity into their way of thinking and working – organisational hierarchy, business impact analysis, roles and responsibilities, practices and processes, management systems, reporting structures, RACI etc. Cybersecurity is seen as a technical function that is best carried out by technology experts.\nEvery organisation has a complex mix of internal and external workforce and other stakeholders, each having their own specific roles, functions, backgrounds, competence and limitations. However, everyone understands the concept of business resilience and can therefore grasp the concept of cyber resilience. Hence, it would be appropriate to convert cybersecurity discussions into cyber resilience discussions, to enable the participation of all stakeholders. This calls for\nA common vocabulary that business and technical stakeholders can both use to clearly communicate their respective perspectives, both orally and in writing, without ambiguity and misunderstanding, A shared understanding leads to faster and better convergence of perspectives and improving the decision process at all levels. A cyber resilience framework that harmonises business, IT and cybersecurity perspectives, as also government and regulatory guidelines, directives and global best practices. This site attempts to address the two themes mentioned above. A common vocabulary is provided through a glossary of terms and definitions that both business and technical users can use. The chapters and topics address various aspects of cyber resilience, covering institutional structures, people, policies, procedures, practices and processes, technology and collaboration. Cybersecurity is addressed as an enabler of speed, safety and resilience in protection, detection, response, and recovery to attacks on our cyberspace, especially the attacks on our critical sectors.\nMany of the insights and perspectives are derived from the practices and processes adopted by organisations having a high level of cybersecurity capability and maturity. Organisations should take this documentation as a generic framework that is further adapted for their specific requirements.\nContact For queries, suggestions and feedback, please contact the site administrator at admin@tipf.in.",
+    "description": "Purpose The purpose of this site is to inform and guide the leadership, management and the composite workforce of critical sector entities (CSEs) on various aspects related to the use, security and resilience of their digital ecosystem. The content is also useful to entities of other sectors too.\nBackground Over the last two decades, India has transformed itself into a highly digitalised nation that has embedded Information and Communication Technology (ICT), Operational Technology (OT) and Internet of Things (IoT) deeply into almost all aspects of our lives. We now have a vibrant national cyberspace, which manifests through a digital ecosystem formed by geographically dispersed, interconnected and federated conglomeration of systems, networks, data repositories, applications, devices, processes and services.",
     "tags": [],
-    "title": "Home",
+    "title": "Welcome !",
     "uri": "/index.html"
   },
   {
     "breadcrumb": "Home",
-    "content": "",
-    "description": "",
+    "content": "Please select the required menu item to read the documentation",
+    "description": "Documentation Section",
     "tags": [],
-    "title": "Categories",
-    "uri": "/categories/index.html"
+    "title": "Overview of Documentation",
+    "uri": "/docs/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Documentation",
+    "content": "",
+    "description": "Documentation related to entities at organisational level",
+    "tags": [],
+    "title": "Organisation related documentation",
+    "uri": "/docs/organisation/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Documentation",
+    "content": "",
+    "description": "Documentation related to use of IT by entities",
+    "tags": [],
+    "title": "Documentation related to use of IT",
+    "uri": "/docs/ict/index.html"
   },
   {
     "breadcrumb": "Home",
-    "content": "Under Construction",
-    "description": "Contact details",
+    "content": "",
+    "description": "Notes Section",
     "tags": [],
-    "title": "Contact",
-    "uri": "/contact/index.html"
+    "title": "Notes",
+    "uri": "/notes/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Documentation",
+    "content": "",
+    "description": "Documentation related to cybersecurity of entities",
+    "tags": [],
+    "title": "Documentation related to cybersecurity",
+    "uri": "/docs/security/index.html"
   },
   {
     "breadcrumb": "Home",
     "content": "",
-    "description": "FAQs",
+    "description": "FAQs Section",
     "tags": [],
-    "title": "FAQs",
+    "title": "Frequently Asked Questions",
     "uri": "/faqs/index.html"
   },
   {
+    "breadcrumb": "Home \u003e Documentation",
+    "content": "",
+    "description": "Glossary of terms and definitions used in this site",
+    "tags": [],
+    "title": "Glossary of Terms and Definitions",
+    "uri": "/docs/glossary/index.html"
+  },
+  {
     "breadcrumb": "Home",
-    "content": "Under construction",
-    "description": "Downloadable resources",
+    "content": "",
+    "description": "Downloadable Resources Section",
     "tags": [],
     "title": "Resources",
     "uri": "/resources/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "Discussions",
+    "tags": [],
+    "title": "Discussions",
+    "uri": "/discussions/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "List of Contacts",
+    "tags": [],
+    "title": "Contacts",
+    "uri": "/contact/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Notes",
+    "uri": "/tags/notes/index.html"
   },
   {
     "breadcrumb": "Home",
@@ -62,5 +110,23 @@ var relearn_searchindex = [
     "tags": [],
     "title": "Tags",
     "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Notes",
+    "content": "Welcome to Notes",
+    "description": "Welcome to Notes",
+    "tags": [
+      "Notes"
+    ],
+    "title": "Welcome !",
+    "uri": "/notes/2025-08-15-12-00/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
   }
 ]
